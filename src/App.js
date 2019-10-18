e27e914fb28065f408cc89e9b97de17859be27b1
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2019-10-18 07:07:02 +0400

    personalise app

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>ALC 4.0 Cloud Challenge I</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Challenge accepted
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jochen.kirstaetter.name/share-your-journey-150daysofalc4/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more...
         </a>
       </header>
     </div>
